cfe3627e9f31a92562263d96f0fc83a9e6e3ad08
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-03-02 11:32:08 +0530

    navbar updated

--- frontend/src/components/megaMenu/MegaMenu.jsx
+++ frontend/src/components/megaMenu/MegaMenu.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './megaMenu.css'
+import { Container } from 'react-bootstrap'
 
 const MegaMenu = () => {
   return (
     <div className='megamenu__section'>
+      <Container>
+        
+      </Container>
       
     </div>
   )
